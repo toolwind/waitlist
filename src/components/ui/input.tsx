8a6b0fd4d7c8c08f26dfa32a3732272f55378ac4
Brandon McConnell <brandon@dreamthinkbuild.com>
2025-05-08 01:55:13 -0400

    Force contrast on autofilled fields

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,6 +11,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
         "file:text-foreground placeholder:text-rose-950/50 dark:placeholder:text-rose-50/50 selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] border-rose-950/25 dark:border-rose-100/15",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        "autofill:!bg-background autofill:!text-foreground",
         className
       )}
       {...props}
